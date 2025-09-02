f26fd41976f5d17b19c280cfecd408d0f466e499
YuzukaAraoka <araokany@gmail.com>
2025-09-02 11:14:07 +0900

    （デザイン）

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     
   resources :tweets
- get 'tweets/new' => 'tweets#new'
- get 'tweets' => 'tweets#index'
- resources :perfumes
+  get 'tweets/new' => 'tweets#new'
+  get 'tweets' => 'tweets#index'
+ resources :users, only: [:show]
+  resources :perfumes
+ 
   root 'tweets#index'
   
 end
